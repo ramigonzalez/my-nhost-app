8ebb78553a8ba8ae707e76a6f31ae840a3495bc1
LAPTOP-QOOVCJT1\Dudley Dursley <ramiro.gonzalez@sandbox.game>
2022-11-23 00:24:07 -0300

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import Layout from './components/Layout';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
+import Layout from "./components/Layout";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import { NhostReactProvider } from "@nhost/react";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+
+import { nhost } from "./utils/nhost";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-      <Toaster />
-    </>
+    <NhostReactProvider nhost={nhost}>
+      <NhostApolloProvider nhost={nhost}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="sign-up" element={<SignUp />} />
+            <Route path="sign-in" element={<SignIn />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Layout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Dashboard />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+      </NhostApolloProvider>
+    </NhostReactProvider>
   );
 }
 
