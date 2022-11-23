51f76eb506ea06aba0f1007c30843f1186ad2a0c
LAPTOP-QOOVCJT1\Dudley Dursley <ramiro.gonzalez@sandbox.game>
2022-11-23 00:24:59 -0300

    user mutation

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,81 @@
-import styles from '../styles/pages/Profile.module.css';
+import styles from "../styles/pages/Profile.module.css";
 
-import { useState } from 'react';
-import { Helmet } from 'react-helmet';
-import { useOutletContext } from 'react-router-dom';
-import Input from '../components/Input';
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import { useOutletContext } from "react-router-dom";
+import Input from "../components/Input";
+
+import { gql, useMutation } from "@apollo/client";
+import { toast } from "react-hot-toast";
+
+import { nhost } from "../utils/nhost";
+
+const UPDATE_USER_MUTATION = gql`
+  mutation ($id: uuid!, $displayName: String!, $metadata: jsonb) {
+    updateUser(
+      pk_columns: { id: $id }
+      _set: { displayName: $displayName, metadata: $metadata }
+    ) {
+      id
+      displayName
+      metadata
+    }
+  }
+`;
+
 
 const Profile = () => {
+  const [mutateUser, { loading: updatingProfile }] =
+    useMutation(UPDATE_USER_MUTATION);
+
   const { user } = useOutletContext();
 
-  const [firstName, setFirstName] = useState(user?.metadata?.firstName ?? '');
-  const [lastName, setLastName] = useState(user?.metadata?.lastName ?? '');
+  const [firstName, setFirstName] = useState(user?.metadata?.firstName ?? "");
+  const [lastName, setLastName] = useState(user?.metadata?.lastName ?? "");
+  const [functionResponse, setFunctionResponse] = useState("<EMPTY>");
+
+  const [imageUrl, setAvatarUrl] = useState(null);
+  const [uploading, setUploading] = useState(false);
 
   const isFirstNameDirty = firstName !== user?.metadata?.firstName;
   const isLastNameDirty = lastName !== user?.metadata?.lastName;
   const isProfileFormDirty = isFirstNameDirty || isLastNameDirty;
 
-  const updateUserProfile = async e => {
+  const updateUserProfile = async (e) => {
     e.preventDefault();
+
+    try {
+      await mutateUser({
+        variables: {
+          id: user.id,
+          displayName: `${firstName} ${lastName}`.trim(),
+          metadata: {
+            firstName,
+            lastName,
+          },
+        },
+      });
+      toast.success("Updated successfully", { id: "updateProfile" });
+    } catch (error) {
+      toast.error("Unable to update profile", { id: "updateProfile" });
+    }
+  };
+
+  const callFunction = async (e) => {
+    const { res, error } = await nhost.functions.call(`test?name=${firstName}`);
+    console.log("FunctionResponse", res.data.data);
+    setFunctionResponse(res.data.data);
+  };
+
+  const uploadImage = async (e) => {
+    setUploading(true);
+
+    if (!e.target.files || e.target.files.length === 0) {
+      throw new Error("You must select an image to upload.");
+    }
+    const file = e.target.files[0];
+    const { fileMetadata, error } = await nhost.storage.upload({ file });
+    console.log("fileMetadata", fileMetadata);
   };
 
   return (
@@ -30,27 +89,36 @@ const Profile = () => {
           <h2>Profile</h2>
           <p>Update your personal information.</p>
         </div>
-
+        <div>
+          <button
+            type="submit"
+            className={styles.button}
+            onClick={callFunction}
+          >
+            Test Cloud Function
+          </button>
+          <p>{functionResponse}</p>
+        </div>
         <div className={styles.card}>
           <form onSubmit={updateUserProfile} className={styles.form}>
-            <div className={styles['form-fields']}>
-              <div className={styles['input-group']}>
+            <div className={styles["form-fields"]}>
+              <div className={styles["input-group"]}>
                 <Input
                   type="text"
                   label="First name"
                   value={firstName}
-                  onChange={e => setFirstName(e.target.value)}
+                  onChange={(e) => setFirstName(e.target.value)}
                   required
                 />
                 <Input
                   type="text"
                   label="Last name"
                   value={lastName}
-                  onChange={e => setLastName(e.target.value)}
+                  onChange={(e) => setLastName(e.target.value)}
                   required
                 />
               </div>
-              <div className={styles['input-email-wrapper']}>
+              <div className={styles["input-email-wrapper"]}>
                 <Input
                   type="email"
                   label="Email address"
@@ -58,9 +126,18 @@ const Profile = () => {
                   readOnly
                 />
               </div>
+              <div className={styles["input-email-wrapper"]}>
+                <Input
+                  type="file"
+                  id="single"
+                  accept="image/*"
+                  onChange={uploadImage}
+                  disabled={uploading}
+                />
+              </div>
             </div>
 
-            <div className={styles['form-footer']}>
+            <div className={styles["form-footer"]}>
               <button
                 type="submit"
                 disabled={!isProfileFormDirty}
